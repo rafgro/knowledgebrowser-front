2e1f99bc1a7361f855e42da71687693250b5040f
rafgro <rafal.grochala@op.pl>
2019-06-06 13:28:42 +0100

    Forgot about commiting, this is big one

--- app.js
+++ app.js
@@ -16,23 +16,35 @@ app.get('/search', function (req, res) {
   
   let hrstart = process.hrtime();
 
-  if( req.query.q.length > 0 ) {
-    queryApi.doYourJob( req.query.q.replace("+"," ") )
+  if( req.query.q !== undefined ) {
+    let query = req.query.q.replace(/\+/g," ");
+    queryApi.doYourJob( query, req.query.offset || 0 )
     .then( results => {
 
       let hrend = process.hrtime(hrstart);
       res.render('preprint-search',
-        { "message": [ { "text": "Execution time: "+(hrend[1] / 1000000 + hrend[0] * 1000).toFixed(2)+" ms" } ],
-          "publication": results } );
+        { "message": [ { "text": "Execution time: "+(hrend[1] / 1000000 + hrend[0] * 1000).toFixed(2)
+                                 +" ms. Found "+results.numberofall+" results." } ],
+          "publication": results.pubs,
+          "title": query+" - Knowledge Browser",
+          "searchquery": query,
+          "pagination_prev_activity": results.pagination.prev_activity,
+          "pagination_prev_link": results.pagination.prev_link,
+          "pagination_next_activity": results.pagination.next_activity,
+          "pagination_next_link": results.pagination.next_link,
+          "pagination": results.pagination.pages } );
         
     })
     .catch( e=> {
       res.render('preprint-search',
-        { "message": [ { "text": e } ] } );
+        { "message": [ { "text": e } ],
+        "title": query+" - Knowledge Browser",
+        "searchquery": query } );
     });
   } else {
     res.render('preprint-search',
-        { "message": [ { "text": "Hi" } ] } );
+        { "message": [ { "text": "Hi!" } ],
+        "title": "Knowledge Browser" } );
   }
 
 });
--- queryApi.js
+++ queryApi.js
@@ -1,11 +1,11 @@
 
 const request = require('request');
 
-exports.doYourJob = function( query ) {
+exports.doYourJob = function( query, offset=0 ) {
 
     return new Promise( ( resolve, reject ) => {
 
-        request('http://knowbro-env.223darfg3a.us-east-2.elasticbeanstalk.com:3000/api/search?q='+query,
+        request('http://knowbro-env.223darfg3a.us-east-2.elasticbeanstalk.com:3000/api/search?q='+query+'&offset='+offset,
           {timeout: 20000}, (error, response, body) => {
 
             if( error ) {
@@ -15,16 +15,53 @@ exports.doYourJob = function( query ) {
                 let toResolve = [];
                 let today = Date.now();
                 let results = JSON.parse(body).results;
+                let allresults = JSON.parse(body).numberofall;
                 
                 if( results != null ) {
                     if( results.hasOwnProperty('message') ) { reject("No results."); }
                     else {
+
                         results.forEach( element => {
                             let thatDate = (new Date(element.date)).getTime();
                             let days = (today - thatDate) / 86400000;
-                            toResolve.push( { "title": element.title, "abstract": element.abstract, "date": days.toFixed(0)+" days ago" } );
+                            toResolve.push( { "title": element.title, "abstract": element.abstract,
+                              "date": days.toFixed(0)+" days ago", "relevancy": element.relativeWeight+"/10 relevant" } );
                         } );
-                        resolve( toResolve );
+
+                        let paginationObj = {
+                            prev_link: '#',
+                            prev_activity: '',
+                            next_link: '#',
+                            next_activity: '',
+                            pages: new Array()
+                        };
+                        let intOffset = parseInt(offset);
+                        let origQuery = query.replace(/ /g,'+');
+                        if( intOffset == 0 ) { paginationObj.prev_link = '#'; paginationObj.prev_activity = 'disabled'; }
+                        else { paginationObj.prev_link = 'http://knowbrofront-env.cnpkrtkwe6.us-east-2.elasticbeanstalk.com/search?q='+origQuery+'&offset='+(intOffset-10); }
+                        if( intOffset+10 >= allresults ) { paginationObj.next_link = '#'; paginationObj.next_activity = 'disabled'; }
+                        else { paginationObj.next_link = 'http://knowbrofront-env.cnpkrtkwe6.us-east-2.elasticbeanstalk.com/search?q='+origQuery+'&offset='+(intOffset+10); }
+                        let maxButtons = 0;
+                        let maxButtonsLimit = 9;
+                        let starter = 1;
+                        if( intOffset > 75 && allresults > 100 ) { starter = 9; maxButtonsLimit = 10; }
+                        if( intOffset > 175 && allresults > 200 ) { starter = 19; maxButtonsLimit = 10; }
+                        if( intOffset > 275 && allresults > 300 ) { starter = 29; maxButtonsLimit = 10; }
+                        if( intOffset > 375 && allresults > 400 ) { starter = 39; maxButtonsLimit = 10; }
+                        for( let i = starter; i < ((allresults/10)+1); i++ ) {
+                            let whatActivity = '';
+                            if( (intOffset+10)/10 == i ) whatActivity = 'active';
+                            let whatLink = '';
+                            if( i == 1 && whatActivity != 'active' ) whatLink = 'http://knowbrofront-env.cnpkrtkwe6.us-east-2.elasticbeanstalk.com/search?q='+origQuery;
+                            else if( whatActivity != 'active' ) whatLink = 'http://knowbrofront-env.cnpkrtkwe6.us-east-2.elasticbeanstalk.com/search?q='+origQuery+'&offset='+10*(i-1);
+
+                            paginationObj.pages.push( { activity: whatActivity, number: i, link: whatLink } );
+
+                            maxButtons++;
+                            if( maxButtons > maxButtonsLimit ) break;
+                        }
+
+                        resolve( { pubs: toResolve, numberofall: allresults, pagination: paginationObj } );
                     }
                 }
                 else {
