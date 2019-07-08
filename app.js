4434d16a7a1dc83ad8a8a69fee5e7fbd655118cb
rafgro <rafal.grochala@op.pl>
2019-07-08 20:28:13 +0100

    A lot of work in accounts and notifications config

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ const express = require('express'),
 
 const app = express();
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({
+  helpers: {
+    if_eq: function(a, b, opts) { if(a == b) { return opts.fn(this); } else { return opts.inverse(this); } }
+  }
+}));
 app.set('view engine', 'handlebars');
 
 app.use('/assets', express.static('assets'));
@@ -79,8 +83,14 @@ passport.deserializeUser(function(user, done) {
 app.get('/signup', function(request,response) {
   let forWhat = '';
   if (request.query.for != undefined) forWhat = '?for=' + request.query.for;
-  if (request.isAuthenticated()) response.redirect('/account' + forWhat);
-  else response.render( 'register', {layout: 'pseudomodal', title: "Sign up - kb:preprints", forWhat: request.query.for } );
+  if (request.isAuthenticated()) {
+    response.redirect('/account' + forWhat);
+    return;
+  }
+  else {
+    response.render( 'register', {layout: 'pseudomodal', title: "Sign up - kb:preprints", forWhat: request.query.for } );
+    return;
+  }
 });
 app.post('/signup', function(request,response) {
   let forWhat = '';
@@ -88,30 +98,39 @@ app.post('/signup', function(request,response) {
 
   if (request.body.password != request.body.password2) {
     response.render( 'register', {layout: 'pseudomodal', title: "Sign up - kb:preprints", error: "Provided passwords are different.", email: request.body.email, forWhat: request.query.for } );
+    return;
   }
   userApi.signup(request.body.email, request.body.password, request.query.for)
     .then((r) => {
-      console.log(r);
+      // console.log(r);
       request.login([{email:request.body.email}], function(err) {
         if (err) {
           console.log(err);
           response.render( 'login', {layout: 'pseudomodal', "title": "Login - kb:preprints", error: 'Sorry, we\'ve encountered an error.', email:request.body.email, forWhat: request.query.for} );
+          return;
         } else {
           response.redirect('/account' + forWhat);
+          return;
         }
       })(request,response,next);
     })
     .catch((e) => {
       e = JSON.parse(e);
       response.render( 'register', {layout: 'pseudomodal', title: "Sign up - kb:preprints", error: e.message, email: request.body.email, forWhat: request.query.for } );
+      return;
     });
-  //response.render( 'register', {layout: 'pseudomodal', "title": "Sign up - kb:preprints"} );
 });
 app.get('/login', function(request,response) {
   let forWhat = '';
   if (request.query.for != undefined) forWhat = '?for=' + request.query.for;
-  if (request.isAuthenticated()) response.redirect('/account' + forWhat);
-  else response.render( 'login', {layout: 'pseudomodal', "title": "Login - kb:preprints", forWhat: request.query.for} );
+  if (request.isAuthenticated()) {
+    response.redirect('/account' + forWhat);
+    return;
+  }
+  else {
+    response.render( 'login', {layout: 'pseudomodal', "title": "Login - kb:preprints", forWhat: request.query.for} );
+    return;
+  }
 });
 app.post('/login', function(request,response,next) {
   let forWhat = '';
@@ -119,48 +138,196 @@ app.post('/login', function(request,response,next) {
   passport.authenticate('local', function(err, user, info) {
     if (err) {
       response.render( 'login', {layout: 'pseudomodal', "title": "Login - kb:preprints", error: JSON.parse(err).message, email:request.body.email, forWhat: request.query.for } );
-      return 0;
+      return;
     }
     request.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000;
     request.login(user, function(err) {
       if (err) {
         console.log(err);
         response.render( 'login', {layout: 'pseudomodal', "title": "Login - kb:preprints", error: 'Sorry, we\'ve encountered an error.', email:request.body.email, forWhat: request.query.for } );
+        return;
       } else {
         response.redirect('/account' + forWhat);
+        return;
       }
     });
   })(request, response, next);
 });
 app.get('/account', function(request,response) {
-  // TODO: get info from api about user notifications
-  console.log(request.user);
-  if (request.isAuthenticated()) response.render( 'account', {"title": "Account - kb:preprints", layout: "accountlayout", username:request.user[0].email} );
-  else response.redirect('/login');
+  if (request.query.for != undefined) {
+    response.redirect('/account/add-notification?for=' + request.query.for);
+    return;
+  }
+  if (request.isAuthenticated()) {
+    let messages = [];
+    if (request.query.m != undefined) {
+      switch(request.query.m) {
+        case 'sm':
+          messages.push({ color:'success', content: 'Notification successfully modified.' });
+          break;
+        case 'sc':
+          messages.push({ color:'success', content: 'Notification successfully created.' });
+          break;
+        case 'sd':
+          messages.push({ color:'success', content: 'Notification successfully deleted.' });
+          break;
+      }
+    }
+    const fewQueries = [ userApi.getAllNotifications(request.user[0].email), userApi.getMailStatus(request.user[0].email)];
+    Promise.all(fewQueries)
+      .then((res) => {
+        // console.log(res);
+        if (res[1] == '0') {
+          messages.push({ color:'warning', content: 'You need to confirm your mail before we can send you notifications.' });
+        }
+        if (res[0].length < 20) {
+          response.render( 'account', {"title": "Account - kb:preprints", layout: "accountlayout", username:request.user[0].email,error:"You have no notifications.", messages} );
+          return;
+        } else {
+          let parsed = JSON.parse(res[0]);
+          parsed = parsed.map((value) => {
+            value.queryText = value.query.toUpperCase();
+            switch(value.frequency) {
+              case 1: value.frequencyText = 'as soon as possible'; break;
+              case 2: value.frequencyText = 'max once a day'; break;
+              case 3: value.frequencyText = 'max once a few days'; break;
+              case 4: value.frequencyText = 'max once a week'; break;
+              case 5: value.frequencyText = 'max once a few weeks'; break;
+            }
+            value.createdText = value.created.replace('T',' ').substring(0,16) + ' UTC';
+            return value;
+          });
+          response.render( 'account', {"title": "Account - kb:preprints", layout: "accountlayout", username:request.user[0].email, notifications: parsed, messages} );
+          return;
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        response.render( 'account', {"title": "Account - kb:preprints", layout: "accountlayout", username:request.user[0].email, error:e.message, messages} );
+        return;
+      });
+  }
+  else {
+    response.redirect('/login');
+    return;
+  }
 });
-app.get('/account/modify-notification', function(request,response) {
-  if (request.isAuthenticated()) response.render( 'account-modifynotification', {"title": "Modify notification - kb:preprints", layout: "accountlayout", username:request.user[0].email} );
-  else response.redirect('/login');
+app.post('/account/modify-notification', function(request,response) {
+  if (request.isAuthenticated()) {
+    if (request.body.purpose === '0') {
+      response.render( 'account-modifynotification', {"title": "Modify notification - kb:preprints", layout: "accountlayout", username:request.user[0].email, created:request.body.created, query:request.body.query, minrelevance:request.body.minrelevance, frequency:request.body.frequency, where:request.body.where, hiddenid:request.body.hiddenid, createdText:request.body.createdText } );
+      return;
+    } else if (request.body.purpose === '1') {
+      // console.log(request.body);
+      userApi.updateNotification(request.user[0].email, request.body.query, request.body.minrelevance, request.body.frequency, request.body.where, request.body.created, request.body.hiddenid)
+        .then(() => {
+          response.redirect('/account?m=sm');
+          return;
+        })
+        .catch((e) => {
+          response.render( 'account-modifynotification', {"title": "Modify notification - kb:preprints", layout: "accountlayout", username:request.user[0].email, created:request.body.created, query:request.body.query, minrelevance:request.body.minrelevance, frequency:request.body.frequency, where:request.body.where, hiddenid:request.body.hiddenid, error: 'Sorry, we\'ve encountered an error.' } );
+          return;
+        });
+    }
+  }
+  else {
+    response.redirect('/login');
+    return;
+  }
+});
+app.post('/account/delete-notification', function(request,response) {
+  if (request.isAuthenticated()) {
+    if (request.body.purpose === '0') {
+      response.render( 'account-deletenotification', {"title": "Delete notification - kb:preprints", layout: "accountlayout", username:request.user[0].email, query:request.body.query, hiddenid:request.body.hiddenid } );
+      return;
+    } else if (request.body.purpose === '1') {
+      userApi.deleteNotification(request.user[0].email, request.body.hiddenid)
+        .then(() => {
+          response.redirect('/account?m=sd');
+          return;
+        })
+        .catch((e) => {
+          response.render( 'account-deletenotification', {"title": "Modify notification - kb:preprints", layout: "accountlayout", username:request.user[0].email, query:request.body.query, hiddenid:request.body.hiddenid, error: 'Sorry, we\'ve encountered an error.' } );
+          return;
+        });
+    }
+  }
+  else {
+    response.redirect('/login');
+    return;
+  }
 });
 app.get('/account/add-notification', function(request,response) {
-  if (request.isAuthenticated()) response.render( 'account-addnotification', {"title": "Add notification - kb:preprints", layout: "accountlayout", username:request.user[0].email} );
-  else response.redirect('/login');
+  if (request.isAuthenticated()) {
+    response.render( 'account-addnotification', {"title": "Add notification - kb:preprints", layout: "accountlayout", username:request.user[0].email, forWhat:request.query.for} );
+    return;
+  }
+  else {
+    response.redirect('/login');
+    return;
+  }
+});
+app.post('/account/add-notification', function(request,response) {
+  if (request.isAuthenticated()) {
+    const parsed = request.body;
+    // console.log(request.user[0].email);
+    userApi.addOneNotification(request.user[0].email, parsed.keyword, parsed.relevance, parsed.frequency, request.user[0].email)
+      .then(() => {
+        response.redirect('/account?m=sc');
+        return;
+      })
+      .catch((e) => {
+        response.render( 'account-addnotification', {"title": "Add notification - kb:preprints", layout: "accountlayout", username:request.user[0].email, forWhat:request.query.for, error:'Sorry, we\'ve encountered an error.'} );
+        return;
+      });
+  }
+  else {
+    response.redirect('/login');
+    return;
+  }
 });
 app.get('/account/settings', function(request,response) {
-  if (request.isAuthenticated()) response.render( 'account-settings', {"title": "Settings - kb:preprints", layout: "accountlayout", username:request.user[0].email} );
-  else response.redirect('/login');
+  if (request.isAuthenticated()) {
+    response.render( 'account-settings', {"title": "Settings - kb:preprints", layout: "accountlayout", username:request.user[0].email} );
+    return;
+  }
+  else {
+    response.redirect('/login');
+    return;
+  }
 });
 app.get('/account/contact', function(request,response) {
-  if (request.isAuthenticated()) response.render( 'account-contact', {"title": "Contact - kb:preprints", layout: "accountlayout", username:request.user[0].email} );
-  else response.redirect('/login');
+  if (request.isAuthenticated()) {
+    response.render( 'account-contact', {"title": "Contact - kb:preprints", layout: "accountlayout", username:request.user[0].email} );
+    return;
+  }
+  else {
+    response.redirect('/login');
+    return;
+  }
 });
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
+  return;
+});
+
+
+app.get('/account/confirm', function(request,response) {
+  userApi.confirmUser(request.query.mail)
+    .then(() => {
+      response.render( 'login', {layout: 'pseudomodal', "title": "Login - kb:preprints", forWhat: request.query.for, error: 'Thank you, mail confirmed!'} );
+      return;
+    })
+    .catch((e) => {
+      response.render( 'login', {layout: 'pseudomodal', "title": "Login - kb:preprints", forWhat: request.query.for, error: 'Sorry, we cannot confirm your mail.'} );
+      return;
+    });
 });
 
 app.get('/preprints/last-week', function(request,response) {
   response.render( 'preprint-weekfeed', {"title": "Last week in science - kb:preprints"} );
+  return;
 });
 
 app.get('/preprints/search', function (req, res) {
@@ -173,8 +340,14 @@ app.get('/preprints/search', function (req, res) {
       res.render('preprint-weekfeed', {"message":[{"text":"We are showing you the summary of preprints in the last week, because your query suggests general request."}],"title": "Last week in science - kb:preprints","searchquery":query});
       let query = 'redirected: ' + req.query.q.replace(/\+/g," ");
       queryApi.doYourJob( query, req.query.offset || 0, req.query.stats || 1, req.query.sort || 0 )
-        .then(() => console.log('redirected to general'))
-        .catch((e) => console.log(e));
+        .then(() => {
+          console.log('redirected to general');
+          return;
+        })
+        .catch((e) => {
+          console.log(e);
+          return;
+        });
     } else {
       let query = req.query.q.replace(/\+/g," ");
       queryApi.doYourJob( query, req.query.offset || 0, req.query.stats || 1, req.query.sort || 0 )
@@ -206,61 +379,73 @@ app.get('/preprints/search', function (req, res) {
             "irrelevant_card2": results.irrelevantCard2,
             "offset": req.query.offset || 0,
             "sort": req.query.sort || 0 } );
-          
+        return;
       })
       .catch( e=> {
         res.render('preprint-search',
           { "message": [ { "text": e } ],
           "title": query+" - kb:preprints",
           "searchquery": query } );
+        return;
       });
     }
   } else {
     res.render( 'preprint-search', { "title": "Knowledge Browser: Preprints", "message": [ { "text": "Please enter your query." }] } );
+    return;
   }
 
 });
 
 app.get('/preprints/terms-and-privacy', function(req,res) {
   res.render( 'preprint-sub-privacypolicy', { "title": "Privacy policy & Terms of use - kb:preprints" } );
+  return;
 });
 app.get('/preprints/about', function(req,res) {
   res.render( 'preprint-sub-about', { "title": "About - kb:preprints" } );
+  return;
 });
 
 app.get('/generate/stats', function(req,res) {
   stats.doYourJob().then( results => {
     res.send( results );
+    return;
   })
   .catch( e=> {
     res.send( e.toString() );
+    return;
   })
 });
 
 app.get('/generate/weekfeed', function(req,res) {
   feedGenerator.doYourJob('week').then( results => {
     res.send( results );
+    return;
   })
   .catch( e=> {
     res.send( e.toString() );
+    return;
   })
 });
 
 app.get('/preprints/sitemap.xml', function(req,res) {
   res.send( seoSitemap.xmlFile() );
+  return;
 });
 
 app.get('/stats-internal-blackseo', function(req,res) {
   stats2.doYourJob().then( results => {
     res.render( 'preprint-search', { "title": "Internal", "message": results } );
+    return;
   })
   .catch( e=> {
     res.send( e.toString() );
+    return;
   });
 });
 
 app.get('*', function(req, res){
   res.render( 'preprint-sub-error', { "title": "Page not found - kb:preprints" } );
+  return;
 });
 
 var port = process.env.PORT || 80;
@@ -268,6 +453,6 @@ var port = process.env.PORT || 80;
 app.listen(port, function () {
   let today = new Date();
   console.log(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()+' server listening on: '+port);
-  stats.doYourJob(); //to update default dummy stats file
-  feedGenerator.doYourJob('week'); //to update default dummy feed file
+  // stats.doYourJob(); //to update default dummy stats file
+  // feedGenerator.doYourJob('week'); //to update default dummy feed file
 });
--- userApi.js
+++ userApi.js
@@ -9,7 +9,7 @@ exports.signup = function (email, pass, firstNotification) {
       }, (error, res, body) => {
       if (error) reject({ errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
       if (res.statusCode == 200) resolve('pozitive');
-      else reject(body || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+      else reject(JSON.parse(body) || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
     })
   });
 };
@@ -23,7 +23,92 @@ exports.login = function (email, pass, newNotification) {
       }, (error, res, body) => {
       if (error) reject({ errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
       if (res.statusCode == 200) resolve('pozitive');
-      else reject(body || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+      else reject(JSON.parse(body) || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+    })
+  });
+};
+
+exports.getAllNotifications = function (email) {
+  return new Promise( (resolve, reject) => {
+    request.post('http://knowbro-env.223darfg3a.us-east-2.elasticbeanstalk.com:3000/api/accounts/allnotifications',
+      {
+          form: { hey: 'ZXVUXb96JPgZVspA', email },
+          timeout: 10000
+      }, (error, res, body) => {
+      if (error) reject({ errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+      if (body.length == 2) reject({ errorType: 'empty', message: 'Empty!'});
+      if (res.statusCode == 200) resolve(body);
+      else reject(JSON.parse(body) || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+    })
+  });
+};
+
+exports.addOneNotification = function (account, keywords, relevance, frequency, where) {
+  return new Promise( (resolve, reject) => {
+    request.post('http://knowbro-env.223darfg3a.us-east-2.elasticbeanstalk.com:3000/api/accounts/addonenotification',
+      {
+          form: { hey: 'ZXVUXb96JPgZVspA', account, keywords, relevance, frequency, where },
+          timeout: 10000
+      }, (error, res, body) => {
+      if (error) reject({ errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+      if (res.statusCode == 200) resolve(body);
+      else reject(JSON.parse(body) || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+    })
+  });
+};
+
+exports.updateNotification = function (account, keywords, relevance, frequency, where, created, hiddenid) {
+  return new Promise( (resolve, reject) => {
+    request.post('http://knowbro-env.223darfg3a.us-east-2.elasticbeanstalk.com:3000/api/accounts/updatenotification',
+      {
+          form: { hey: 'ZXVUXb96JPgZVspA', account, keywords, relevance, frequency, where, created, hiddenid },
+          timeout: 10000
+      }, (error, res, body) => {
+      if (error) reject({ errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+      if (res.statusCode == 200) resolve(body);
+      else reject(JSON.parse(body) || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+    })
+  });
+};
+
+exports.deleteNotification = function (account, hiddenid) {
+  return new Promise( (resolve, reject) => {
+    request.post('http://knowbro-env.223darfg3a.us-east-2.elasticbeanstalk.com:3000/api/accounts/deletenotification',
+      {
+          form: { hey: 'ZXVUXb96JPgZVspA', account, hiddenid },
+          timeout: 10000
+      }, (error, res, body) => {
+      if (error) reject({ errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+      if (res.statusCode == 200) resolve(body);
+      else reject(JSON.parse(body) || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+    })
+  });
+};
+
+exports.confirmUser = function (key) {
+  return new Promise( (resolve, reject) => {
+    request.post('http://knowbro-env.223darfg3a.us-east-2.elasticbeanstalk.com:3000/api/accounts/confirmuser',
+      {
+          form: { hey: 'ZXVUXb96JPgZVspA', key },
+          timeout: 10000
+      }, (error, res, body) => {
+      if (error) reject({ errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+      if (res.statusCode == 200) resolve(body);
+      else reject(JSON.parse(body) || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+    })
+  });
+};
+
+exports.getMailStatus = function (email) {
+  return new Promise( (resolve, reject) => {
+    request.post('http://knowbro-env.223darfg3a.us-east-2.elasticbeanstalk.com:3000/api/accounts/usermailstatus',
+      {
+          form: { hey: 'ZXVUXb96JPgZVspA', email },
+          timeout: 10000
+      }, (error, res, body) => {
+      if (error) reject({ errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
+      if (res.statusCode == 200) resolve(body);
+      else reject(JSON.parse(body) || { errorType: 'server', message: 'Sorry, we\'ve encountered an error.' });
     })
   });
 };
